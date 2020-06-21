3bbdb2dd1c797d32fb7f23d318c2a229accc2a45
Christopher Dutcher <cadutcher@icloud.com>
2020-06-21 23:35:41 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,11 +3,11 @@ def create_an_empty_array
 end
 
 def create_an_array
-
+Elements = ["Water", "Earth", "Fire", "Air"]
 end
 
 def add_element_to_end_of_array(array, element)
-
+Elements.push("metal")
 end
 
 def add_element_to_start_of_array(array, element)
